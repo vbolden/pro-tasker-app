573c4ca37fcec4e2ac2c75bc66f41e7443039981
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-01 10:42:41 -0400

    added data types, validation rules, and user reference to projectSchema

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema({
-    name: {},
-    description: {},
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
 });
 
 const Project = mongoose.model("Project", projectSchema);
