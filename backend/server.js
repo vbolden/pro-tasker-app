b35be000bb5fc30c2f740689d028c6b8d43ecd89
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-01 10:21:49 -0400

    imported connetDB function and added function call

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3003;
 
+const connectDB = require('./config/db.js');
+
+// DB CONNECTION
+connectDB();
+
 app.get("/test", (req, res) => {
     res.send("Test route");
 });
