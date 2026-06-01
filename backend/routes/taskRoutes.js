6902a098ab54c86273d6e3b9630b64e018e9d840
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-01 16:04:46 -0400

    added routes for updating and deleting a task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -36,11 +36,82 @@ taskRouter.post("/:projectId/tasks", async (req, res) => {
 });
 
 // READ ALL
+taskRouter.get("/:projectId/tasks", async (req, res) => {
+    try {
+        const project = await Project.findOne({
+            _id: req.params.projectId,
+            user: req.user._id
+        });
 
-// READ ONE
+        if (!project) {
+            return res.status(403).json({
+                message: "Unauthorized or project not found."
+            });
+        }
+
+        const tasks = await Task.find({
+            project: req.params.projectId,
+        });
+
+        res.json(tasks);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 // UPDATE 
+taskRouter.put("/:projectId/tasks/:taskId", async (req, res) => {
+    try {
+
+        const project = await Project.findOne({
+            _id: req.params.projectId,
+            user: req.user._id
+        });
+
+        if (!project) {
+            return res.status(403).json({
+                message: "Unauthorized or project not found."
+            });
+        }
+
+        const updatedTask = await Task.findByIdAndUpdate(
+            {
+                _id: req.params.taskId,
+            },
+            req.body,
+        );
+
+        res.json(updatedTask);
+    } catch (error) {
+        res.status(500).send("There was an issue updating the task...");
+    }
+});
 
 // DELETE
+taskRouter.delete("/:projectId/tasks/:taskId", async (req, res) => {
+    try {
+
+        const project = await Project.findOne({
+            _id: req.params.projectId,
+            user: req.user._id
+        });
+
+        if (!project) {
+            return res.status(403).json({
+                message: "Unauthorized or project not found."
+            });
+        }
+
+        const deleted = await Task.findByIdAndDelete({
+            _id: req.params.taskId,
+            project: req.params.projectId
+        });
+
+        res.status(200).json({ message: "Task deleted successfully." });
+    } catch (error) {
+        res.status(500).send("There was an issue deleting the task...");
+    }
+});
 
 module.exports = taskRouter;
