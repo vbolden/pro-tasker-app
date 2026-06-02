da5e52b030f2fe11747c7488009f50def9f9ae46
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-02 14:14:58 -0400

    added basic styling to sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,19 +2,22 @@ function Sidebar() {
     return (
         <aside className="sidebar">
             <h2>ProTasker</h2>
-            <div className="circle"></div>
-            <div className="user-details">
-                <p>User's Name</p>
-                <p>Product Lead</p>
+            <div className="sidebar-header">
+                <div className="circle"></div>
+                <div className="user-details">
+                    <p>User's Name</p>
+                    <span>Product Lead</span>
+                </div>
             </div>
+            <hr />
             <div className="navbar">
                 <ul>
-                    <li>Dashboard</li>
-                    <li>My Projects</li>
-                    <li>My Tasks</li>
-                    <li>Schedule</li>
-                    <li>Messages</li>
-                    <li>Settings</li>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">My Projects</a></li>
+                    <li><a href="#">My Tasks</a></li>
+                    <li><a href="#">Schedule</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">Settings</a></li>
                 </ul>
             </div>
         </aside>
