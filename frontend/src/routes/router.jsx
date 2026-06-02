4e4c6a6bf46432a491271bfa4b530ed32412c723
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-02 15:50:32 -0400

    created a routes.jsx file to swap content when a different route/page is selected

---
+++ frontend/src/routes/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Dashboard from "../pages/Dashboard";
+import Projects from "../pages/Projects";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Dashboard />
+            },
+            {
+                path: "/projects",
+                element: <Projects />
+            }
+        ]
+    }
+]);
