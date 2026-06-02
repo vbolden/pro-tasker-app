479c2d489b8067dd539a3d151e5c1743940ebbe7
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-02 08:54:01 -0400

    created a project auth function to use in task routes

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
+const Project = require("../models/Project.js");
 
 function authMiddleware(req, res, next) {
     try {
         let token = req.headers.authorization;
 
         if (!token) {
-            return res.status(403).json({
+            return res.status(401).json({
                 error: "No token provided"
             });
         }
@@ -21,8 +22,33 @@ function authMiddleware(req, res, next) {
 
     } catch (error) {
         console.log(error.message);
-        res.status(400).json({ message: error.message });
+        res.status(401).json({ message: error.message });
     }
 }
 
-module.exports = authMiddleware;
+async function projectAuth(req, res, next) {
+    try {
+        // FIND PROJECT BY ID
+        const project = await Project.findOne({
+            _id: req.params.projectId,
+            user: req.user._id
+        });
+
+        // CHECK IF OWNED BY USER
+        if (!project) {
+            return res.status(403).json({
+                message: "Unauthorized or project not found."
+            });
+        }
+
+        req.project = project;
+        next();
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+module.exports = {authMiddleware, projectAuth};
