ecc673295efe24c7ce12e30851d889b116335535
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-03 15:08:56 -0400

    added route path for auth page

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -2,10 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Dashboard from "../pages/Dashboard";
 import Projects from "../pages/Projects";
+import Auth from "../pages/Auth";
 
 const router = createBrowserRouter([
     {
         path: "/",
+        element: <Auth />
+    },
+    {
+        path: "/dashboard",
         element: <App />,
         children: [
             {
@@ -13,7 +18,7 @@ const router = createBrowserRouter([
                 element: <Dashboard />
             },
             {
-                path: "/projects",
+                path: "projects",
                 element: <Projects />
             }
         ]
