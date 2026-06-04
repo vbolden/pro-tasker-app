ef46728a864a1371ed1b22e3c83de1fda52924c4
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-04 18:49:39 -0400

    added a function to fetch all projects from the API

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import API from "../api/axios";
 import ProjectCard from "../components/ProjectCard";
 import ProjectModal from "../components/ProjectModal";
 
 function Projects() {
+    const [projects, setProjects] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedProject, setSelectedProject] = useState(null);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const res = await API.get("/projects");
+
+                setProjects(res.data);
+            } catch (error) {
+                console.error("Failed to load projects:", error);
+            }
+        };
+        fetchProjects();
+    }, []);
 
     const handleCreate = () => {
         setSelectedProject(null);
@@ -16,6 +31,23 @@ function Projects() {
         setShowModal(true);
     };
 
+    const handleDelete = async (id) => {
+        const confirmed = window.confirm(
+            'Delete this project?'
+        );
+
+        if (!confirmed) return;
+
+        try {
+            await projectService.deleteProject(id);
+
+            setProjects((prev) =>
+                prev.filter((project) => project._id !== id));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="projects-container">
             <div className="projects-top">
@@ -23,17 +55,17 @@ function Projects() {
                 <button id="new-project" onClick={handleCreate}>+ New Project</button>
             </div>
             <div className="project-grid">
-                <ProjectCard
-                    project={{
-                        id: 1,
-                        title: "Finish MERN Project",
-                        description: 'Create a full stack app that allows user to...'
-                    }}
-                    onEdit={handleEdit}
-                />
+                {projects.map((project) => (
+                    <ProjectCard
+                        key={project._id}
+                        project={project}
+                        onEdit={handleEdit}
+                        onDelete={handleDelete}
+                    />
+                ))}
             </div>
             {showModal && (
-                <ProjectModal 
+                <ProjectModal
                     project={selectedProject}
                     onClose={() => setShowModal(false)}
                 />
