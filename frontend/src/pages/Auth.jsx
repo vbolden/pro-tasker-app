02e022d2aaaf6c43ec46b18b32de8406bf2f8f12
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-04 18:30:53 -0400

    added connection to backend and navigation to dashboard

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,7 +1,48 @@
 import { useState } from "react";
+import API from "../api/axios";
+import { useNavigate } from "react-router-dom";
 
 function Auth() {
-    const [activeTab, setActiveTab] = useState('signup')
+    const [activeTab, setActiveTab] = useState('signup');
+    const [signupData, setSignupData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await API.post("/users/register", signupData);
+            localStorage.setItem("token", res.data.token);
+            navigate("/dashboard");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await API.post("/users/login", loginData);
+
+            localStorage.setItem("token", res.data.token);
+
+            navigate("/dashboard");
+        } catch (error) {
+            console.error(error);
+
+        }
+    };
 
     return (
         <div className="auth-container">
@@ -25,30 +66,30 @@ function Auth() {
                     <div className="tab-content">
                         {activeTab === "signup" && (
                             <div id="signup">
-                            <h1>Sign Up for Free</h1>
+                                <h1>Sign Up for Free</h1>
 
-                            <form action="/" method="post">
-                                <div className="top-row">
-                                    <input type="text" placeholder="First Name" required />
-                                    <input type="text" placeholder="Last Name" required />
-                                </div>
-                                <div className="bottom-row">
-                                    <input type="text" placeholder="Email" required />
-                                    <input type="text" placeholder="Username" required />
-                                    <input type="password" placeholder="Password" required />
-                                    <input type="password" placeholder="Confirm Password" required />
-                                    <input type="submit" value="Register" className="button" />
-                                </div>
-                            </form>
-                        </div>
-                    )}
+                                <form onSubmit={handleRegister}>
+                                    <div className="top-row">
+                                        <input type="text" placeholder="First Name" required />
+                                        <input type="text" placeholder="Last Name" required />
+                                    </div>
+                                    <div className="bottom-row">
+                                        <input type="text" placeholder="Email" value={signupData.email} onChange={(e) => setSignupData({ ...signupData, email: e.target.value })} required />
+                                        <input type="text" placeholder="Username" value={signupData.username} onChange={(e) => setSignupData({ ...signupData, username: e.target.value })} required />
+                                        <input type="password" placeholder="Password" value={signupData.password} onChange={(e) => setSignupData({ ...signupData, password: e.target.value })} required />
+                                        {/* <input type="password" placeholder="Confirm Password" required /> */}
+                                        <input type="submit" value="Register" className="button" />
+                                    </div>
+                                </form>
+                            </div>
+                        )}
 
                         {activeTab === "login" && (<div id="login">
                             <h1>Welcome Back</h1>
 
-                            <form action="/" method="post">
-                                <input type="text" placeholder="Username" />
-                                <input type="password" placeholder="Password" />
+                            <form onSubmit={handleLogin}>
+                                <input type="text" placeholder="Username" value={loginData.username} onChange={(e) => setLoginData({ ...loginData, username: e.target.value })} required />
+                                <input type="password" placeholder="Password" value={loginData.password} onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} required />
                                 <p className="forgot"><a href="#">Forgot Password?</a></p>
                                 <input type="submit" value="Log In" className="button" />
                             </form>
