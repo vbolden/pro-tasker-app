38ceea359305beee8cfcd3d7e6a9dc9804f3150e
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-04 11:20:28 -0400

    updated project card to pass project data and callbacks as props

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,14 +1,26 @@
 import { FaTrash } from 'react-icons/fa';
 import { FaEdit } from 'react-icons/fa';
 
-function ProjectCard() {
+function ProjectCard({ project, onEdit, onDelete }) {
     return (
         <div className="card-body">
-            <h3>Finish MERN Project</h3>
-            <p>Create a full stack app that allows user to...</p>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+
             <div className="project-btns">
-                <button id="edit">Edit <FaEdit /></button>
-                <button id="delete">Delete <FaTrash /></button>
+                <button
+                    id="edit"
+                    onClick={() => onEdit(project)}
+                >
+                    Edit <FaEdit />
+                </button>
+
+                <button
+                    id="delete"
+                    onClick={() => onDelete(project._id)}
+                >
+                    Delete <FaTrash />
+                </button>
             </div>
         </div>
     );
