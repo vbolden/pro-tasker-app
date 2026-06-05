cc3fc69e54906bcda386af6b8cbd64710de83abf
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-04 21:12:43 -0400

    changed styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
+import { BsFillBellFill } from "react-icons/bs";
+
+
 function Header () {
     return (
         <header>
             <div className="header-content">
                 <h1>Hello, User!</h1>
                 <div className="header-right">
-                    <button id="notifications">🔔</button>
+                    <button id="notifications"> <BsFillBellFill style={{ width: "20px", height: "20px" }} /> </button>
                     <input type="text" placeholder="Search" />
                     <a href="#">
                         <div className="circle"></div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,20 +1,59 @@
-function Dashboard () {
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+
+function Dashboard() {
     return (
-        <div className="dash-wrapper">
-            <div className="dash-left">
-                <div className="schedule">
-                    <p>Upcoming Schedule</p>
+        <div className='dashboard'>
+            <div className="dash-wrapper">
+                <div className="dash-left">
+                    <div className="schedule">
+                        <p>Upcoming Schedule</p>
+                        <div className="schedule-content">
+                            <div className="calendar">
+                                <Calendar />
+                            </div>
+                            <div className="upcoming-events">
+                                <div className="event-card">
+                                    <h4>Project Review</h4>
+                                    <span>2:00 PM</span>
+                                </div>
+                                <div className="event-card">
+                                    <h4>Sprint Planning</h4>
+                                    <span>4:00 PM</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="projects">
+                        <p>Recent Projects</p>
+                    </div>
+                    <div className="tasks">
+                        <p>Recent Tasks</p>
+                    </div>
                 </div>
-                <div className="projects">
-                    <p>Recent Projects</p>
-                </div>
-                <div className="tasks">
-                    <p>Recent Tasks</p>
+                <div className="dash-right">
+                    <div id="project-stats">
+                        <p>Project Count</p>
+                        <span className='big-number'>6</span>
+                    </div>
+                    <div id="tasks-done">
+                        <p>Tasks Done</p>
+                        <span>10/20</span>
+                        <div className='progress'>
+                            <CircularProgressbar value={50} />
+                        </div>
+                    </div>
+                    <div id="stats">
+                        <p>On Track</p>
+                        <div className='progress'>
+                            <CircularProgressbar value={90} />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="dash-right"></div>
         </div>
-    )
+    );
 }
 
 export default Dashboard;
