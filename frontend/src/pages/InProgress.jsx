7dfc9866770895bc12b04d21dd47aa14643b6959
Valerie Bolden <valerie.bolden@gmail.com>
2026-06-06 12:52:59 -0400

    added an in progress page for additional links

---
+++ frontend/src/pages/InProgress.jsx
@@ -0,0 +1,11 @@
+function ComingSoon({title}) {
+    return (
+        <div className="coming-soon">
+            <span>🚧</span>
+            <h1>{title}</h1>
+            <p>This section is coming soon.</p>
+        </div>
+    );
+}
+
+export default ComingSoon;
